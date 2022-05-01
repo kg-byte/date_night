a98e96e8fbee3f648fce006254b548e960bdff15
Kim G <xiaole.guo@yahoo.com>
2022-05-01 12:01:08 -0600

    insert method adds nodes and returns the depth of added node

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -1,17 +1,37 @@
+require 'node'
+
 class BinarySearchTree
+  attr_accessor :root, :depth
   def initialize
-  	@nodes = Hash.new
+    @root = nil
   end
+
   def insert(score, movie)
-  	  node = nodes[:movie] = score
-  	  depth_of(node)
+    depth = 0
+  	if @root == nil 
+      @root = Node.new(score, movie)
+      depth = 0
+    else 
+      current_node = @root
+      previous_node = @root
+
+      while current_node != nil 
+        previous_node = current_node
+        if current_node.data.values[0] > score 
+          current_node = previous_node.left
+          depth +=1
+        else 
+          current_node = previous_node.right
+          depth +=1
+        end
+      end
+      if previous_node.data.values[0] > score
+        previous_node.left = Node.new(score, movie)
+      else
+        previous_node.right = Node.new(score, movie)
+      end
+    end
+    depth
   end
-
-  def depth_of(node)
-  	index = nodes.find_index(node)
-  	
-  end
-
-
 
 end
--- spec/binary_search_tree_spec.rb
+++ spec/binary_search_tree_spec.rb
@@ -6,5 +6,15 @@ RSpec.describe BinarySearchTree do
 
   let(:tree) {BinarySearchTree.new}
 
+  it 'exists' do 
+    expect(tree).to be_a BinarySearchTree
+  end
+
+  it 'inserts nodes and returns the depth of current node' do 
+    expect(tree.insert(61, "Bill & Ted's Excellent Adventure")).to eq 0
+    expect(tree.insert(16, "Johnny English")).to eq 1
+    expect(tree.insert(92, "Sharknado 3")).to eq 1
+    expect(tree.insert(50, "Hannibal Buress: Animal Furnace")).to eq 2
+  end
 
 end
